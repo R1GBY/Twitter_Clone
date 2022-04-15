ee3eabefc359fd6a23b5255e559889d033cd4c0d
Umut Arda <umutardakapan@gmail.com>
2022-04-16 01:12:16 +0300

    post history

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-    alert("It worked")
+    $.get("/api/posts", (results) =>{
+        outputPosts(results, $(""))
+    })
 })
+
+function outputPosts(results, container) {
+    container.html("");
+
+    results.forEach(result => {
+        var html = createPostHtml(result);
+        container.append(html);
+    });
+
+    if(results.length === 0){
+        container.append("<span class='noResults'> Nothing to Show.</span>")
+    }
+}
